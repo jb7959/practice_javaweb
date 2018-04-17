72f5f4558b38d6d6b709cc92259f09d6b44cff4b
jb7959 <jb7959@naver.com>
2018-04-17 15:52:09 +0900

    init

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # 실습을 위한 개발 환경 세팅
-* https://github.com/slipp/web-application-server 프로젝트를 자신의 계정으로 Fork한다. Github 우측 상단의 Fork 버튼을 클릭하면 자신의 계정으로 Fork된다.
-* Fork한 프로젝트를 eclipse 또는 터미널에서 clone 한다.
-* Fork한 프로젝트를 eclipse로 import한 후에 Maven 빌드 도구를 활용해 eclipse 프로젝트로 변환한다.(mvn eclipse:clean eclipse:eclipse)
-* 빌드가 성공하면 반드시 refresh(fn + f5)를 실행해야 한다.
+* IntelliJ IDEA
+* Maven
 
 # 웹 서버 시작 및 테스트
 * webserver.WebServer 는 사용자의 요청을 받아 RequestHandler에 작업을 위임하는 클래스이다.
@@ -15,19 +13,75 @@
 * 각 요구사항을 구현하는 것이 중요한 것이 아니라 구현 과정을 통해 학습한 내용을 인식하는 것이 배움에 중요하다. 
 
 ### 요구사항 1 - http://localhost:8080/index.html로 접속시 응답
-* 
+
+스트림혹은 파일은 각자의 Class가 Java에 존재한다. 여기서 각 각체를 읽어주는 Reader가 별도로 존재한다.
+각 리더는 버퍼를 사용하면 효율적으로 읽을 수 있는 것으로 보인다.
+따라서 InputStream이나 File의 각 Reader(InputStreamReader, FileReader)는 BufferReader를 사용 할 수 있다.
+
+        public class BufferedReader extends Reader {
+        ....
+        public BufferedReader(Reader in) {
+            this(in, defaultCharBufferSize);
+        }
+    
+        /** Checks to make sure that the stream has not been closed */
+        private void ensureOpen() throws IOException {
+            if (in == null)
+                throw new IOException("Stream closed");
+        } ....
+        
+ BufferReader는 Reader의 확장이며, InputStreamReader 또한 Reader의 확장이다.
+ FileReader는 InputStreamReader의 확장이다.
+ 
+     public class InputStreamReader extends Reader{... }
+     public class FileReader extends InputStreamReader {...}
+     
+* 참고
+* https://stackoverflow.com/a/35446009 [Read/convert an InputStream to a String]
+* http://jdm.kr/blog/3 [자바파일입출력]
+* https://www.mkyong.com/java/how-to-convert-file-into-an-array-of-bytes/ [NIO를 이용한 파일에서 바이트배열로의 변경]
 
 ### 요구사항 2 - get 방식으로 회원가입
-* 
+* 없음 
 
 ### 요구사항 3 - post 방식으로 회원가입
-* 
+* Chr(13) = \r == 캐리지리턴 : 동일줄 첫번째 자리로 커서 이동
+* Chr(10) = \n == 라인피드 : 현재 커서가 위치한 곳에서 아래로 한줄 내림
+
+--> 유닉스 계열의 문서편집기에선 char(10)만 사용해도 줄바꿈이 되지만, 윈도우계열은 두 바이트(\r\n)가 함께해야 줄바꿈이 수행됨
 
 ### 요구사항 4 - redirect 방식으로 이동
-* 
+* 웹 브라우저는 이전 HTTP의 요청사항을 보유하고 있음.
+* HTTP 302를 통한 redirect 방식으로 이동은 의도치 않은 data 재전송 및 조작을 방지 할 수 있음
 
 ### 요구사항 5 - cookie
-* 
+* cookie 삽입을 위해서는 HTTP Header에 "SET-Cookie: "CookieName" = "value(쿠키값)";의 방식으로 기입해주어야 함
+
+        private void response302Header(DataOutputStream dos, String location, Map<String,String> cookies) {
+            try {
+                dos.writeBytes("HTTP/1.1 302 FOUND \r\n");
+                dos.writeBytes("LOCATION: " + location + "\r\n");
+                if(!cookies.isEmpty()){
+                    String setCookie = "SET-Cookie: ";
+                    for(String key : cookies.keySet()){
+                        setCookie+=(key+"="+cookies.get(key));
+                    }
+                    dos.writeBytes(setCookie + "\r\n");
+                }
+            } catch (IOException e) {
+                log.error(e.getMessage());
+            }
+        }
+* Map의 경우 모든 항목의 조회는 Map.keySet()을 활용하여 확인 할 수 있다.
+
+  - Map.keySet을 통해서 모든 맵의 KeySet을 추출한다. 
+    - (Map의 ID는 중복이 되면 안되기에, SET의 중복배제사상과 동일하다.)
+  - 추출된 식별자 Set은 Foreach()를 통해 참조할 MAP의 전체를 반복조회한다.
+
+        for(String key : cookies.keySet()){
+            setCookie+=(key+"="+cookies.get(key));
+        }
+                        
 
 ### 요구사항 6 - stylesheet 적용
 * 
--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,139 +2,163 @@ package webserver;
 
 import java.io.*;
 import java.net.Socket;
-import java.net.URLDecoder;
 import java.nio.file.Files;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
-import db.DataBase;
 import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import util.IOUtils;
-import webserver.http.ReadingFileContent;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
-
+    private static String defaultPath = "./webapp";
     private Socket connection;
+    private static Map<String,User> users = new HashMap<String,User>(); //DB
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
     }
 
     public void run() {
-        log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
-                connection.getPort());
-
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-            String contents = "";
-            BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
-            //readLine() 은 개행문자 직전까지 한줄 읽고 포인터를 다음으로 이동.
-            String line = br.readLine();
-            log.debug("request line :{}", line);
-            if (line == null) {
-                return;
+            DataOutputStream dos = new DataOutputStream(out);
+
+            ///////////////////////HTTP REQUEST 해석//////////////////////////////////////
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            String  req = IOUtils.readData(reader,connection.getReceiveBufferSize());
+            String [] headAndBody = req.split("\\r\\n\\r\\n"); //줄바꿈을 기준으로 분리(한줄공백)
+            String httpHead = headAndBody[0];
+            String httpBody ="";
+            log.debug("HttpHead:::::::::::::::::::::::::{}",req);
+            httpHead.split("\\s+"); //띄어쓰기를 기준으로 분류
+
+            //todo Http Model 만들어서 해결 할 것 18.04.16
+            String method = httpHead.split("\\s+")[0];
+            String uri = httpHead.split("\\s+")[1];
+            String httpVersion = httpHead.split("\\s+")[2];
+            uri= defaultPath+uri;
+
+            if(method.equals("POST")){
+                httpBody = headAndBody[1];
             }
 
-            String[] tokens = line.split(" ");
-            int contentLength = 0;
-            String cookie = "";
-            while (!line.equals("")) {
-                log.debug("header : {}", line);
-                line = br.readLine();
-                if (line.contains("Content-Length")) {
-                    contentLength = getContentLength(line);
+            ///////////////////////HTTP REQUEST 해석//////////////////////////////////////
+
+            /*회원가입요청 /user/create */
+            if(uri.split("\\?")[0].equals(defaultPath+"/user/create")){
+                Map<String,String> map = null;
+                //get일 경우
+                if(method.equals("GET")){
+                    map = getParam(uri);
+                }else if(method.equals("POST")){
+                    map = getParam(httpBody);
                 }
-                if (line.contains("Cookie")) {
-                    cookie = getContentsValue(line);
-                }
+                User user = new User(map.get("userId"),map.get("password"),map.get("name"),map.get("email"));
+                log.info("추가된 유저 {}",user.toString());
+                users.put(user.getUserId(),user);
+                uri = "/user/list.html"; //완료후 LIST 페이지로it노조
+                response302Header(dos,uri);
+                responseBody(dos,new byte[0]);
             }
+            /*회원가입요청끝*/
 
+            /*로그인요청 /user/login */
+            if(uri.split("\\?")[0].equals(defaultPath+"/user/login")){
+                Map<String,String> map = null;
 
-            String requestLine = line; // HTTP 요청라인
-            String httpsMethod = tokens[0];
-            String uri = tokens[1];
-            String param = null;
-            if (uri.contains("?")) {
-                param = URLDecoder.decode(uri.substring(uri.indexOf("?")));
-                uri = uri.substring(0, uri.indexOf("?"));
-            }
+                uri = "/user/login_failed.html";
+                Map cookies = new HashMap();
+                cookies.put("logined","false");
 
-//http GET 처리
-            if (httpsMethod.equals("GET")) {
+                //get일 경우
+                if(method.equals("GET")){
+                    uri = "/user/login.html";
+                }else if(method.equals("POST")){
+                    map = getParam(httpBody);
 
-                if (param == null) { //http 파라미터가 없는경우
-                    if (uri.equals("/user/list")) {
-                        if (IsLogined(cookie)) {
-                            for (User user : DataBase.findAll()) {
-                                log.info("id :{}", user.getUserId());
-                                log.info("name :{}", user.getName());
-                            }
+                    if(users.containsKey(map.get("userId"))){
+                        String userId = map.get("userId");
+                        String password = map.get("password");
 
+                        //Login ID와 pw가 같다면 성공
+                        if(users.get(userId).getPassword().equals(password)){
+                            //Login Success
+                            log.info("로그인 성공!");
+                            uri = "/user/list.html"; //완료후 LIST 페이지로
+                            cookies.remove ("logined");
+                            cookies.put("logend","true");
+                            response302Header(dos,uri,cookies);
+                            responseBody(dos,new byte[0]);
+                        }else{
+                            log.info("비밀번호를 확인해주세요.");
+                            uri = "/user/login_failed.html";
                         }
-                    } else {
-                        contents = new ReadingFileContent().read(uri);
-                        responseResource(out, uri);
+                    }else{ //ID가 없을 때..
+                        log.info("존재하지 않는 ID 입니다. ID 입력을 확인해주세요.");
+                        uri = "/user/login_failed.html";
                     }
-                } else {
-                    // /user/create 처리
-
-                    if (uri.equals("/user/create")) {
-                        Map<String, String> params = getParamsFromURI(param);
-                        User user = new User(params.get("userID"), params.get("password"), params.get("name"), params.get("email"));
-                        log.info("helloWorld");
-                        log.info(user.toString());
-                    }
-
+                    response302Header(dos,uri);
+                    responseBody(dos,new byte[0]);
                 }
-            } else if (httpsMethod.equals("POST")) {
-                if (uri.equals("/user/create")) {
-                    String body = IOUtils.readData(br, contentLength);
-                    Map<String, String> params = httpsBodyParser(body);
-                    params.get("userID");
-                    User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
-                    log.info(user.toString());
-                    DataBase.addUser(user);
-                    DataOutputStream dos = new DataOutputStream(out);
-                    response302Header(dos, "/index.html");
 
-                } else if (uri.equals("/user/login")) {
-                    String body = IOUtils.readData(br, contentLength);
-                    Map<String, String> params = httpsBodyParser(body);
-                    if ((DataBase.findUserById(params.get("userId")) != null)) {
-                        if (DataBase.findUserById(params.get("userId")).getPassword().equals(params.get("password"))) {
-                            DataOutputStream dos = new DataOutputStream(out);
-                            response302LoginedHeader(dos, "/index.html");
-                        }
-                    } else {
-                        responseResource(out, "/user/login_failed.html");
-                    }
-                } else {
-                    responseResource(out, uri);
+            }
+
+            /*로그인요청끝 /user/login */
+            byte[] body = Files.readAllBytes(new File(uri).toPath()); //NIO를 활용한 File to byte[]body
+            response200Header(dos, body.length);
+            responseBody(dos, body);
+
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private Map getParam(String paramString){
+        String param = paramString;
+        if(paramString.contains("?")){
+            param = paramString.split("\\?")[1];
+        }
+
+        Map<String,String> map = new HashMap<>();
+
+        for(String p:param.split("&")){
+            String [] temp = p.split("=");
+            map.put(temp[0].trim(),temp[1].trim());
+        }
+        return map;
+    }
+
+    private void response302Header(DataOutputStream dos, String location) {
+        try {
+            dos.writeBytes("HTTP/1.1 302 FOUND \r\n");
+            dos.writeBytes("LOCATION: " + location + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private void response302Header(DataOutputStream dos, String location, Map<String,String> cookies) {
+        try {
+            dos.writeBytes("HTTP/1.1 302 FOUND \r\n");
+            dos.writeBytes("LOCATION: " + location + "\r\n");
+            if(!cookies.isEmpty()){
+                String setCookie = "SET-Cookie: ";
+                for(String key : cookies.keySet()){
+                    setCookie+=(key+"="+cookies.get(key));
                 }
-            } else {
-                log.info("*****************************" + br.readLine());
-                responseResource(out, uri);
+                dos.writeBytes(setCookie + "\r\n");
             }
         } catch (IOException e) {
             log.error(e.getMessage());
         }
     }
 
-    private void responseResource(OutputStream out, String uri) throws IOException {
-        DataOutputStream dos = new DataOutputStream(out);
-        byte[] body = Files.readAllBytes(new File("./webapp" + uri).toPath());
-        String contentType = "html";
-        if(uri.contains("css")){contentType = "css";}
-        response200Header(dos, body.length, contentType);
-        responseBody(dos, body);
-    }
-
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
-
             dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
@@ -143,41 +167,6 @@ public class RequestHandler extends Thread {
         }
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent, String contentType) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            if (contentType.equals("css")) {
-                dos.writeBytes("Content-Type: text/css;charset=utf-8\r\n");
-            } else {
-                dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            }
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-    }
-
-    private void response302Header(DataOutputStream dos, String uri) {
-        try {
-            dos.writeBytes("HTTP/1.1 302 Redirect \r\n");
-            dos.writeBytes("Location: " + uri + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-    }
-
-    private void response302LoginedHeader(DataOutputStream dos, String uri) {
-        try {
-            dos.writeBytes("HTTP/1.1 302 Redirect \r\n");
-            dos.writeBytes("Set-Cookie: logined=true \r\n");
-            dos.writeBytes("Location: " + uri + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-    }
 
     private void responseBody(DataOutputStream dos, byte[] body) {
         try {
@@ -187,52 +176,4 @@ public class RequestHandler extends Thread {
             log.error(e.getMessage());
         }
     }
-
-    private Map<String, String> getParamsFromURI(String param) {
-        Map devidedParams = new HashMap<String, String>();
-        String[] params = param.split("&");
-        for (String p : params) {
-            log.info(p);
-            String temp[] = p.split("\\=");
-            devidedParams.put(temp[0], temp[1]);
-        }
-        return devidedParams;
-    }
-
-    private int getContentLength(String line) {
-        return Integer.parseInt(getContentsValue(line));
-    }
-
-    private String getContentsValue(String line) {
-        String[] headerTokens = line.split(":");
-        return headerTokens[1].trim();
-    }
-
-    private Map httpsBodyParser(String body) throws UnsupportedEncodingException {
-        String[] devidedString = body.split("&");
-        Map returnMap = new HashMap<String, String>();
-        for (String e : devidedString) {
-            String[] temp = e.split("=");
-            returnMap.put(temp[0], URLDecoder.decode(temp[1], "UTF-8"));
-        }
-        log.info("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@Here");
-        return returnMap;
-    }
-
-    private Boolean IsLogined(String cookie) {
-        Boolean result = false;
-        if (cookie.contains("logined=true")) {
-            log.info("로그인 됨");
-            result = true;
-        } else if (cookie.contains("logined=false")) {
-            log.info("로그아웃 상태");
-        } else {
-            log.info("로그인 및 로그아웃 기록 쿠키 부재");
-        }
-
-        return result;
-    }
-
 }
-
-
